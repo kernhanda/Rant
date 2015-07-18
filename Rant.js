907ff1c69bb6cc290f2aa21a47aa78bc39ca945f
kernhanda <kern.handa@gmail.com>
2015-07-18 16:39:28 -0700

    Initial work on Rant.

---
+++ Rant.js
@@ -0,0 +1,115 @@
+/// <reference path="typings/qunit/qunit.d.ts"/>
+/// <reference path="typings/node/node.d.ts"/>
+/// <reference path="typings/seedrandom/seedrandom.d.ts"/>
+var seedrandom = require("seedrandom");
+var Rant;
+(function (Rant) {
+    function isFunction(func) {
+        return func && typeof func === "function";
+    }
+    var RantEngine = (function () {
+        function RantEngine(seed) {
+            this.seed = seed;
+            this.rngStack = [];
+            this.currentRng = typeof seed !== 'undefined' ? seedrandom(seed + '') : seedrandom();
+        }
+        RantEngine.prototype.pushSeed = function (seed) {
+            if (typeof seed !== 'undefined') {
+                this.rngStack.push(this.currentRng);
+                this.currentRng = seedrandom(seed + '');
+            }
+        };
+        RantEngine.prototype.popSeed = function (seed) {
+            if (typeof seed !== 'undefined') {
+                this.currentRng = this.rngStack.pop();
+            }
+        };
+        RantEngine.prototype.Fixed = function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i - 0] = arguments[_i];
+            }
+            var argsCopy = [];
+            args = argsCopy.concat.apply(argsCopy, args);
+            return function (seed) {
+                _this.pushSeed(seed);
+                argsCopy = [];
+                for (var i = 0; i < args.length; i++) {
+                    var arg = args[i];
+                    if (isFunction(arg)) {
+                        arg = arg();
+                    }
+                    if (Array.isArray(arg)) {
+                        argsCopy = argsCopy.concat(arg);
+                    }
+                    else {
+                        argsCopy.push(arg);
+                    }
+                }
+                _this.popSeed(seed);
+                if (argsCopy.length == 1) {
+                    return argsCopy[0];
+                }
+                else {
+                    return argsCopy;
+                }
+            };
+        };
+        RantEngine.prototype.Pick = function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i - 0] = arguments[_i];
+            }
+            var argsCopy = [];
+            args = argsCopy.concat.apply(argsCopy, args);
+            return function (seed) {
+                _this.pushSeed(seed);
+                var val = args[Math.floor(_this.currentRng.quick() * args.length)];
+                if (isFunction(val)) {
+                    val = (val());
+                }
+                _this.popSeed(seed);
+                return val;
+            };
+        };
+        RantEngine.prototype.Shuffle = function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i - 0] = arguments[_i];
+            }
+            var picked = [];
+            var argsCopy = [];
+            argsCopy = argsCopy.concat.apply(argsCopy, args);
+            return function (seed) {
+                _this.pushSeed(seed);
+                var interestedLength = argsCopy.length - picked.length;
+                console.log("argsCopy.length: " + argsCopy.length);
+                console.log("picked.length: " + picked.length);
+                console.log("interestedLength:" + interestedLength);
+                if (interestedLength <= 0) {
+                    picked.length = 0;
+                    interestedLength = argsCopy.length;
+                }
+                var pickedIndex = Math.floor(_this.currentRng.quick() * interestedLength);
+                picked.push(pickedIndex);
+                var val = argsCopy[pickedIndex];
+                argsCopy[pickedIndex] = argsCopy[interestedLength - 1];
+                argsCopy[pickedIndex - 1] = val;
+                if (isFunction(val)) {
+                    val = val();
+                }
+                _this.popSeed(seed);
+                return val;
+            };
+        };
+        return RantEngine;
+    })();
+    Rant.RantEngine = RantEngine;
+})(Rant || (Rant = {}));
+var rant = new Rant.RantEngine();
+exports.Fixed = Rant.RantEngine.prototype.Fixed.bind(rant);
+exports.Pick = Rant.RantEngine.prototype.Pick.bind(rant);
+exports.Shuffle = Rant.RantEngine.prototype.Shuffle.bind(rant);
---
+++ Rant.ts
@@ -0,0 +1,115 @@
+/// <reference path="typings/qunit/qunit.d.ts"/>
+/// <reference path="typings/node/node.d.ts"/>
+/// <reference path="typings/seedrandom/seedrandom.d.ts"/>
+
+import seedrandom = require("seedrandom");
+module Rant {
+interface RantExpression<T> {
+    (seed?: number | string): T | T[];
+}
+
+export type Ranting<T> = T | T[] | RantExpression<T>;
+
+function isFunction(func) {
+  return func && typeof func === "function";
+}
+
+export class RantEngine {
+  private currentRng: prng;
+  private rngStack: prng[] = [];
+  constructor(private seed?: number | string) {
+    this.currentRng = typeof seed !== 'undefined' ? seedrandom(seed + '') : seedrandom();
+  }
+
+  private pushSeed(seed?: number | string) {
+    if (typeof seed !== 'undefined') {
+      this.rngStack.push(this.currentRng);
+      this.currentRng = seedrandom(seed + '');
+    }
+  }
+
+  private popSeed(seed?: number | string) {
+    if (typeof seed !== 'undefined') {
+      this.currentRng = this.rngStack.pop();
+    }
+  }
+
+  Fixed<T>(...args: Ranting<T>[]): RantExpression<T> {
+    let argsCopy: T[] = [];
+    args = argsCopy.concat.apply(argsCopy, args);
+    return (seed?: number | string): T | T[] => {
+      this.pushSeed(seed);
+
+      argsCopy = [];
+      for (let i = 0; i < args.length; i++) {
+        let arg = args[i];
+        if (isFunction(arg)) {
+          arg = (<RantExpression<T>>arg)();
+        }
+        if (Array.isArray(arg)) {
+          argsCopy = argsCopy.concat(arg);
+        } else {
+          argsCopy.push(<T>arg);
+        }
+      }
+
+      this.popSeed(seed);
+
+      if (argsCopy.length == 1) {
+        return argsCopy[0];
+      } else {
+        return argsCopy;
+      }
+    }
+  }
+
+  Pick<T>(...args : Ranting<T>[]) : RantExpression<T> {
+    let argsCopy: T[] = [];
+    args = argsCopy.concat.apply(argsCopy, args);
+    return (seed?: number | string): T | T[] => {
+      this.pushSeed(seed);
+      let val:T|T[]|RantExpression<T> = args[Math.floor(this.currentRng.quick() * args.length)];
+      if (isFunction(val)) {
+        val = <T|T[]>((<RantExpression<T>>val)());
+      }
+      this.popSeed(seed);
+
+      return <T|T[]>val;
+    }
+  }
+
+  Shuffle<T>(...args : Ranting<T>[]) : RantExpression<T> {
+      let picked: number[] = [];
+      let argsCopy: T[] | T[][] | RantExpression<T>[] = [];
+      argsCopy = argsCopy.concat.apply(argsCopy, args);
+      return (seed?: number | string): T | T[] => {
+        this.pushSeed(seed);
+        let interestedLength = argsCopy.length - picked.length;
+        console.log("argsCopy.length: " + argsCopy.length);
+        console.log("picked.length: " + picked.length);
+        console.log("interestedLength:" + interestedLength);
+        if (interestedLength <= 0) {
+          picked.length = 0;
+          interestedLength = argsCopy.length;
+        }
+        let pickedIndex = Math.floor(this.currentRng.quick() * interestedLength);
+        picked.push(pickedIndex);
+        let val:T|T[]|RantExpression<T> = argsCopy[pickedIndex];
+        argsCopy[pickedIndex] = argsCopy[interestedLength - 1];
+        argsCopy[pickedIndex - 1] = val;
+
+        if (isFunction(val)) {
+          val = (<RantExpression<T>>val)();
+        }
+
+        this.popSeed(seed);
+
+        return <T|T[]>val;
+      }
+  }
+}
+}
+let rant = new Rant.RantEngine();
+export let Fixed = Rant.RantEngine.prototype.Fixed.bind(rant);
+export let Pick = Rant.RantEngine.prototype.Pick.bind(rant);
+export let Shuffle = Rant.RantEngine.prototype.Shuffle.bind(rant);
